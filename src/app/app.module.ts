5addf664005676b7957c07b7bb4b23ce21511feb
ttzv <tomasz.zwak@gmail.com>
2021-08-15 09:10:06 +0200

    Add Okta auth, add protected route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { SalesPersonListComponent } from './sales-person-list/sales-person-list.
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
@@ -17,7 +17,33 @@ import { CartDetailsComponent } from './components/cart-details/cart-details.com
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { LoginComponent } from './components/login/login.component';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
+
+import {
+  OKTA_CONFIG,
+  OktaAuthModule,
+  OktaCallbackComponent,
+  OktaAuthGuard
+} from '@okta/okta-angular';
+
+import myAppConfig from './config/my-app-config';
+import { ProductService } from './services/product.service';
+import { MembersPageComponent } from './components/members-page/members-page.component';
+
+const oktaConfig = Object.assign({
+  onAuthRequired: (oktaAuth: any, injector: { get: (arg0: any) => any; }) => {
+    const router = injector.get(Router);
+
+    // Redirect the user to your custom login page
+    router.navigate(['/login']);
+  }
+}, myAppConfig.oidc);
+
 const routes: Routes = [
+  {path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard]},
+  {path: 'login/callback', component: OktaCallbackComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'cart-details', component: CartDetailsComponent},
   {path: 'checkout', component: CheckoutComponent},
   {path: 'search/:keyword', component: ProductListComponent},
@@ -39,7 +65,10 @@ const routes: Routes = [
     ProductDetailsComponent,
     CartStatusComponent,
     CartDetailsComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    LoginComponent,
+    LoginStatusComponent,
+    MembersPageComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
@@ -47,9 +76,10 @@ const routes: Routes = [
     AppRoutingModule,
     HttpClientModule,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OktaAuthModule
   ],
-  providers: [],
+  providers: [ProductService, { provide: OKTA_CONFIG, useValue: oktaConfig }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+import myAppConfig from 'src/app/config/my-app-config';
+import * as OktaSignIn from '@okta/okta-signin-widget'; //need to create src/okta.d.ts anddeclare module explicitly there
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+
+  oktaSignIn: any;
+  constructor(private oktaAuthService: OktaAuthService) { 
+    this.oktaSignIn = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      features: {
+        registration: true
+      },
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true, 
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.oktaSignIn.remove();
+    this.oktaSignIn.renderEl({
+      el: '#okta-sign-in-widget'},
+      (response: { status: string; }) => {
+        if (response.status === 'SUCCESS'){
+          this.oktaAuthService.signInWithRedirect();
+        }
+      },
+      (error: any) => {
+        throw error;
+      }
+    );
+  }
+
+}
